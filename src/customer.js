d64bed872a4fa2a1f7e64af8bbcc36166cba8e8f
athreyapatel <athreyapatel@gmail.com>
2020-01-21 12:22:29 +0530

    webpack config

---
+++ src/customer.js
@@ -0,0 +1,18 @@
+export class Customer {
+    constructor(id,name) {
+        this.id=id;
+        this.name=name;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    } 
+
+
+
+
+}
